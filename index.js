b1c0d8e1c782404e2ab6a8c848f78db5f637b478
codenameransom <codejoshuaroque@gmail.com>
2021-03-19 01:02:11 -0400

    commit tree 9eec911c869a73a414ddc4ae62f636395724d73b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Joshua Roque';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
